a310a87bf4d52a90e53cb2a3eb65fa0ea7f62d14
hahaker <hahaker007@gmail.com>
2025-04-25 21:48:48 +0300

    Update DockerEntrypoint.sh

--- DockerEntrypoint.sh
+++ DockerEntrypoint.sh
@@ -2,17 +2,7 @@
 
 # Start fail2ban
 [ $X_UI_ENABLE_FAIL2BAN == "true" ] && fail2ban-client -x start
-if [ ! -f /etc/x-ui/config.json ]; then
-    mkdir -p /etc/x-ui
-    echo '{
-      "port": ${XUI_PORT:-2053},
-      "ssl": { "enabled": ${XUI_SSL:-false} },
-      "web": {
-        "username": "${XUI_USERNAME:-admin}",
-        "password": "${XUI_PASSWORD:-admin}"
-      }
-    }' > /etc/x-ui/config.json
-fi
+
 
 # Run x-ui
 exec /app/x-ui
